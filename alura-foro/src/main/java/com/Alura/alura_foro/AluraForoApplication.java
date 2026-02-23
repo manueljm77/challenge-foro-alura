bbe79f9cb89fa92621b063547e800acc22b08026
Manuel Jaliffe <manueljaliffe@gmail.com>
2026-02-23 15:27:59 -0600

    primer  commit del proyecto

---
+++ alura-foro/src/main/java/com/Alura/alura_foro/AluraForoApplication.java
@@ -0,0 +1,13 @@
+package com.Alura.alura_foro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraForoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraForoApplication.class, args);
+	}
+
+}
---
+++ alura-foro/src/test/java/com/Alura/alura_foro/AluraForoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Alura.alura_foro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraForoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
